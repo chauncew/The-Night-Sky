eb3a57b3eed2093c92e6b0afaa297fb505024778
chauncew <47641547+chauncew@users.noreply.github.com>
2019-12-30 10:34:55 -0600

    Added all files

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'linear');
+});
+
